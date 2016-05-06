61137af1c0365131ce433233a16c3d3af9c5f01c
legokichi <legokichi@gmail.com>
2016-05-06 18:17:09 +0900

    5D

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+
+/// <reference path="./require.d.ts" />
+
+import React = require('react');
+import ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
+
+console.log("hello webpack");
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+module.exports = {
+  entry: {
+    index: './src/index.tsx'
+  },
+  output: {
+    filename: './dist/index.js'
+  },
+  resolve: {
+    root: [path.join(__dirname,'node_modules')],
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+  },
+  externals: {
+    //don't bundle the 'react' npm package with our bundle.js
+    //but get it from a global 'React' variable
+    'react': 'React'
+  },
+  module: {
+    loaders: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  ts: {
+    configFileName: "./tsconfig.json"
+  }
+}
